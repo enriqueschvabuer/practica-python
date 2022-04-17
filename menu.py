8d955a8fd7daace6b6235fcb51930c3ebb1db175
enriqueschvabuer <enriqueschvabuer@gmail.com>
2022-04-17 19:11:18 -0300

    agregado de opciones

--- menu.py
+++ menu.py
@@ -5,19 +5,48 @@ def conversor (valor_dolar):
     cantidad_moneda = float(input("ingrese cantidad de pesos: "))
     dolares = str(round(cantidad_moneda / valor_dolar, 2))
     print ("tienes $" + dolares + " dolares ")
+
+
+def primos(numero):
+    if numero == 1 or numero == 0:
+        return False
+    else:
+        contador = 0
+    if numero < 4:
+        return True
+    else:
+        for i in range(2,numero):
+            if numero % i == 0:
+                contador += 1
+    if contador == 0:
+        return True
+    else:
+        return False
+
+
+def palindromo(palabra):
+    palabra = palabra.replace(' ', '')
+    palabra = palabra.lower()
+    palabra_invertida = palabra[::-1]
+    if palabra == palabra_invertida:
+        return True
+    else:
+        return False
     
 
 def run():
     respuesta = "s"
     while respuesta == "s" :
-        menu = int(input(""" 
+        menu = input(""" 
         1-Coversor de monedas
-        2-adivina el numero
-        3-Pesos Uruguayos
+        2-Adivina el numero
+        3-Numeros primos
+        4-Palindromos
+        5-Salir
 
         Elige una opción: 
 
-        """))
+        """)
 
 
         if menu == 1:
@@ -63,6 +92,45 @@ def run():
                     break
                 elif respuesta_juego != "s":
                     respuesta_juego=input("Ingresaste una opcion incorrecta.desea continuar (s/n):")
+
+
+        elif menu == 3:
+            respuesta_primos = "s"
+            while respuesta_primos == "s":
+                numero = int(input("ingrese un numero: "))
+                if primos(numero):
+                    print("es primo")
+                else:
+                    print("no es primo")
+                respuesta_primos=input("desea ingresar otro numero (s/n): ")
+                if respuesta_primos == "n" :
+                    break
+                elif respuesta_primos != "s":
+                    respuesta_primos=input("Ingresaste una opcion incorrecta.desea continuar (s/n):")
+
+
+        elif menu == 4:
+            respuesta_palindromos = "s"
+            while respuesta_palindromos == "s":
+                palabra = input('Escribe una palabra: ')
+                es_palindromo = palindromo(palabra)
+                if es_palindromo == True:
+                    print('Es palíndromo')
+                else:
+                    print('No es palíndromo')
+                respuesta_palindromos=input("desea ingresar otra palabra (s/n): ")
+                if respuesta_palindromos == "n" :
+                    break
+                elif respuesta_palindromos != "s":
+                    respuesta_palindromos=input("Ingresaste una opcion incorrecta.desea continuar (s/n):")
+
+
+        elif menu == 5:
+            break
+        
+
+        respuesta = input("ingresaste una opcion inconrrecta. Desea continuar (s/n): ")       
+
 
 
 if __name__ == "__main__":
